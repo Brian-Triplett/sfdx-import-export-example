949a89beeb84a845046c07a8f86bc28b19a558c6
Brian Triplett <brian.triplett@ncino.com>
2018-06-28 12:04:13 -0400

    Updated SOQL

--- create_org.sh
+++ create_org.sh
@@ -4,4 +4,4 @@
 sfdx force:org:create -a export_org -s -f dev-scratch-def.json
 
 # Add some accounts and contacts to the org
-sfdx force:apex:execute -u export_org -f account_contact.apex > apex.log
+sfdx force:apex:execute -u export_org -f account_contact.apex
